eecda71e53c44c9798114e506b63a200c3e5eff1
supriyo <hisupriyooo@gmail.com>
2021-01-12 21:09:21 +0530

    add express router in backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
-import products from "./data/products.js";
 import connectDB from "./config/db.js";
+
+// Routes
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -10,17 +12,7 @@ connectDB();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("api is running...");
-});
-
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/product/:id", (req, res) => {
-  const product = products.find((product) => product._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
